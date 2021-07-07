91f56c596cd2dee9d48ef890b5b9ae532585ec02
Gabriel <Gabriel.l.romero@gmail.com>
2021-07-07 23:10:21 +0000

    Added output for compute

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ function compute()
     let years = document.getElementById("years").value;
     let interest = ((principal * years * rate) / 100);
     let year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerText = 
+        "If you deposit " +principal+", \<br\> at an interest rate of " +rate+"%\<br\>You will receive an amouint of " +amount+", \<br\>in the year " +year+"\<br\>"
 }
 
 function updateRate() {
     let rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval+"%";
 }
         
