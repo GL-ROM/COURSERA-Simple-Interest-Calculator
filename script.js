c4aa096aa105aa63d3e029571785d44092b1c4ca
Gabriel <Gabriel.l.romero@gmail.com>
2021-07-07 23:32:03 +0000

    Adding notes and fixing compute

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Compute the interest by grabbing values from inputs and returning text to a span element
 function compute()
 {
     let principal = document.getElementById("principal").value;
@@ -6,10 +7,9 @@ function compute()
     let interest = ((principal * years * rate) / 100);
     let year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerText = 
-        "If you deposit " +principal+", \<br\> at an interest rate of " +rate+"%\<br\>You will receive an amouint of " +amount+", \<br\>in the year " +year+"\<br\>"
+    document.getElementById("result").innerText = "If you deposit " +principal+", \<br\> at an interest rate of " +rate+"%\<br\>You will receive an amouint of " +amount+", \<br\>in the year " +year+"\<br\>"
 }
-
+// Changes the value of the rate slider "dynamically" by grabbing the value as it changes then setting the current displayed value to that. 
 function updateRate() {
     let rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval+"%";
