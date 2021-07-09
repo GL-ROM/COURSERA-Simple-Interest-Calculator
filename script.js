8a5986008934d95f938316dac9a6a9069c783c9d
Gabriel <Gabriel.l.romero@gmail.com>
2021-07-09 10:13:47 -0400

    Finished up code according to specs.

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ function compute()
     let interest = ((principal * years * rate) / 100);
     let year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerText = "If you deposit " +principal+", \<br\> at an interest rate of " +rate+"%\<br\>You will receive an amouint of " +amount+", \<br\>in the year " +year+"\<br\>"
+    if (principal <= 0) {
+        alert("Please enter a postive number.")
+        document.getElementById("principal").focus()
+    } else {
+        document.getElementById("result").innerHTML = "If you deposit <span class='number'>$" +principal+"</span>,\<br\> at an interest rate of <span class='number'>" +rate+"%</span>\<br\>You will receive an amount of <span class='number'>$" +interest+"</span>,\<br\> in the year <span class='number'>" +year+"</span>\<br\>"
+    }
 }
 // Changes the value of the rate slider "dynamically" by grabbing the value as it changes then setting the current displayed value to that. 
 function updateRate() {
